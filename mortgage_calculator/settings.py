7c4ffacb56a8dbcc0416e17c57fc4a7a09c46687
kam1dere <kam1dere@yandex.ru>
2022-08-24 16:20:49 +0300

    settings+url

--- mortgage_calculator/settings.py
+++ mortgage_calculator/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Сторонние библиотеки
+    'rest_framework',
+
+    # Мои приложения
+    'calculator.apps.CalculatorConfig',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +130,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
+}
