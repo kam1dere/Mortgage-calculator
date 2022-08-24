ff8284897bf384b09995b554265ad9b446d26ce9
kam1dere <kam1dere@yandex.ru>
2022-08-24 16:26:05 +0300

    admin + urls

---
+++ calculator/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from rest_framework import routers
+from .views import BankList, BankViewSet, BankApiList
+
+# path('calculator/', include('calculator.urls')),
+router = routers.SimpleRouter()
+router.register(r'api', BankViewSet)
+urlpatterns = [
+    path('', BankList.as_view(), name='bank_list'),
+    path('DRFList', BankApiList.as_view(), name='bank_list_drf')
+]
+
+urlpatterns += router.urls
