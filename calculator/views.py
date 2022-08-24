a7203b68826655bc3a9cd242fcf18cba17c7b21e
kam1dere <kam1dere@yandex.ru>
2022-08-24 16:29:20 +0300

    CRUD + APIVIEW (DRF)

--- calculator/views.py
+++ calculator/views.py
@@ -34,3 +34,47 @@ class BankList(ListView):
         except ValueError:
             print('Введены неверные данные')
             return queryset
+
+
+# CRUD
+class BankViewSet(viewsets.ModelViewSet):
+    queryset = Bank.objects.all()
+    serializer_class = BankSerializerAPI
+    permission_classes = [permissions.IsAdminUser]
+
+
+# Вывод с использованием ДРФ
+class BankApiList(ListAPIView):
+    serializer_class = BankSerializer
+    permission_classes = [permissions.AllowAny]
+    http_method_names = ['get']
+    ordering_fields = '__all__'
+    ordering = ['rate_min']
+    search_fields = ['name']
+
+    def get_queryset(self):
+        queryset = Bank.objects.all().order_by('rate_min')
+        request = self.request.query_params
+        try:
+            deposit = int(request['deposit'])
+            first_payment = int(request['first_payment'])
+            term = int(request['term'])
+            summa = deposit - first_payment
+            queryset = Bank.objects.filter(Q(payment_min__lte=summa) & Q(payment_max__gte=summa) &
+                                           Q(term_min__lte=term) & Q(term_max__gte=term)).order_by('rate_min')
+            queryset = queryset.annotate(fact_pay=ExpressionWrapper(monthly_payment(summa, term, stavka='rate_min'),
+                                                                    output_field=PositiveIntegerField()))
+            return queryset
+        except MultiValueDictKeyError:
+            print('Введены неверные данные')
+            return queryset
+        except ValueError:
+            print('Введены неверные данные')
+            return queryset
+
+    def get_serializer_context(self):
+        return {
+            'deposit': self.request.query_params['deposit'],
+            'first_payment': self.request.query_params['first_payment'],
+            'term': self.request.query_params['term'],
+        }
