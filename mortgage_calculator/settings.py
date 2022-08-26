349f06c1d521cce1cb628d1f6b0c2f42aaf7be73
kam1dere <kam1dere@yandex.ru>
2022-08-26 15:04:46 +0300

    With Docker

--- mortgage_calculator/settings.py
+++ mortgage_calculator/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-57@*9241!d0!#qy0uvk+13(8=j3j7g&@e*3u1z^h^x@x45is&i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -81,11 +82,11 @@ WSGI_APPLICATION = 'mortgage_calculator.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'banks',
-        'USER': 'kam1dere',
-        'PASSWORD': 'roiadmin',
-        'HOST': 'localhost',
-        'PORT': '',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
